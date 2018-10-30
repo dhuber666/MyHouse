d6aa215a7ca49d6053949d7795d649af159550fe
Huber Dominik <Huberdo@voestalpine.root.local>
2018-10-30 15:24:34 +0100

    todos added

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, OverlayTrigger, Popover, Col, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
@@ -17,19 +17,40 @@ class Rooms extends React.Component {
     return rooms.map(room => <Room {...room} key={room.uid} />)
   }
 
+  popoverTop = () => (
+    <Popover id="popover-positioned-top">
+      <Row>
+        {/* TODO: Create components for adding new room and post for room */}
+        <Col md={6} xs={6}>
+          <strong>Room</strong>
+        </Col>
+        <Col md={6} xs={6}>
+          <strong>Post</strong>
+        </Col>
+      </Row>
+    </Popover>
+  )
+
   render() {
 
 
     return (
       <div style={{ marginTop: 50 }}>
         {/* TODO: implement AddRoom Component */}
-        <Button><FontAwesomeIcon icon={faPlus} /></Button>
+
+        <OverlayTrigger trigger="focus" placement="top" overlay={this.popoverTop()}>
+          <Button><FontAwesomeIcon icon={faPlus} /></Button>
+        </OverlayTrigger>
+
+
 
         {this.renderRooms()}
       </div>
     );
   }
 }
+
+
 
 const mapStateToProps = ({ rooms }) => {
   const roomsArray = _.map(rooms, (value, uid) => {
